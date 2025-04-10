26e263ae244e787722b7b29225756aa99601e0ce
boss7609 <polo861028@gmail.com>
2025-04-10 12:04:51 +0800

    Add files via upload

--- app.js
+++ app.js
@@ -470,17 +470,10 @@ function updateTime() {
 updateTime();
 setInterval(updateTime, 1000);
 
-// 禁止雙指縮放
 document.addEventListener('gesturestart', function (e) {
   e.preventDefault();
 });
 
-// 禁止雙擊縮放
-let lastTouchEnd = 0;
-document.addEventListener('touchend', function (e) {
-  const now = new Date().getTime();
-  if (now - lastTouchEnd <= 300) {
-    e.preventDefault();
-  }
-  lastTouchEnd = now;
-}, false);
+document.addEventListener('dblclick', function (e) {
+  e.preventDefault();
+});
